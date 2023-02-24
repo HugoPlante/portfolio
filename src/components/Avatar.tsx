30e022b93f49f44879adad462a1b0e65752c77c5
Hugo Plante <hugo.plante.2@ulaval.ca>
2023-02-23 19:16:27 -0500

    fixed img import

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import avatarUrl from "../assets/images/profile/avatar.webp";
 
 const Avatar = () => {
   return (
     <img
-      className="w-52 h-52 p-1 rounded-full ring-2 ring-blue-700"
-      src="src\assets\images\profile\avatar.webp"
+      className="w-52 h-52 p-1 rounded-full ring-2 ring-blue-700 dark:ring-blue-700"
+      src={avatarUrl}
       alt="My photo"
     ></img>
   );
