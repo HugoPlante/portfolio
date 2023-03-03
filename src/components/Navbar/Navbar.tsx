337fe0e9b3b7d30690383843e96372a8d9144f36
Hugo Plante <hugo.plante.2@ulaval.ca>
2023-03-03 16:48:16 -0500

    added navbar to translations

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import Link from "./Link";
 
 const Navbar = () => {
+  const { t } = useTranslation("navbar");
   return (
     <nav className="fixed left-0 top-0 w-screen h-12 border-b-2 border-background-variant bg-background-main">
       <ul
         role="list"
         className="flex flex-row justify-center items-center h-full space-x-2 md:space-x-4"
       >
-        <Link text={"Accueil"} href={"#header"} />
-        <Link text={"Description"} href={"#description"} />
-        <Link text={"Expériences"} href={"#experiences"} />
-        <Link text={"Contact"} href={"#contact"} />
+        <Link text={t("home")} href={"#header"} />
+        <Link text={t("aboutMe")} href={"#description"} />
+        <Link text={t("experiences")} href={"#experiences"} />
+        <Link text={t("contact")} href={"#contact"} />
       </ul>
     </nav>
   );
